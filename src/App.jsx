d6ff1a25a9c2ea687ae6d10e856f047a643a63ab
Nigen <Nigen48@gmail.com>
2017-04-06 13:05:31 +0300

    Добавление компонента App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = React.createElement('h1', null, 'React Todo');
-const subtitle = React.createElement('p', { className: 'subtitle' }, 'Это мое первое приложение на React');
-const container = React.createElement('div', null, title, subtitle);
+function App() {
+  return (
+    <div>
+      <h1>React Todo</h1>
+      <p className="subtitle">Это мое первое приложение на React</p>
+    </div>
+  );
+};
 
-const app = (
-  <div>
-    <h1>React Todo</h1>
-    <p className="subtitle">Это мое первое приложение на React</p>
-  </div>
-);
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
