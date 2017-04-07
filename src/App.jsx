39417efd0136550dd7d4236f58732238448abf31
Nigen <Nigen48@gmail.com>
2017-04-07 15:02:24 +0300

    Создана статичная версия приложения

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import todos from './todos';
+import Header from './components/Header';
+import Todo from './components/Todo';
+
 function App(props) {
   return (
     <main>
-      <header>
-        <h1>{props.title}</h1>
-      </header>
+      <Header title={props.title} />
 
       <section className="todo-list">
-        <div className="todo completed">
-          <button className="checkbox icon">
-            <i className="material-icons">check_box</i>
-          </button>
-
-          <span className="todo-title">Изучить JavaScript</span>
-
-          <button className="delete icon">
-            <i className="material-icons">delete</i>
-          </button>
-        </div>
-
-        <div className="todo">
-          <button className="checkbox icon">
-            <i className="material-icons">check_box_outline_blank</i>
-          </button>
-
-          <span className="todo-title">Изучить React</span>
-
-          <button className="delete icon">
-            <i className="material-icons">delete</i>
-          </button>
-        </div>
+        {props.todos.map(todo =>
+          <Todo key={todo.id} title={todo.title} completed={todo.completed} />)
+          }
       </section>
     </main>
   );
 }
 
-ReactDOM.render(<App title="React Todo" />, document.getElementById('root'));
+App.propTypes = {
+  title: React.PropTypes.string,
+  todos: React.PropTypes.arrayOf(React.PropTypes.shape({
+    id: React.PropTypes.number.isRequired,
+    title: React.PropTypes.string.isRequired,
+    completed: React.PropTypes.bool.isRequired
+  })).isRequired
+};
+
+App.defaultProps = {
+  title: 'React Todo'
+};
+
+ReactDOM.render(<App todos={todos}/>, document.getElementById('root'));
