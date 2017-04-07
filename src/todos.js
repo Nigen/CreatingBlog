8b35eb3d3278a2fb52069803a668f0aae5f0ad19
Nigen <Nigen48@gmail.com>
2017-04-07 14:42:08 +0300

    Перемещение списка задач в массив

---
+++ src/todos.js
@@ -0,0 +1,22 @@
+const todos = [
+  {
+    title: 'Изучить JS',
+    completed: true
+  },
+  {
+    title: 'Изучить React',
+    completed: false
+  },
+  {
+    title: 'Написать приложение',
+    completed: false
+  }
+];
+
+export default todos;
+
+
+
+
+
+]
