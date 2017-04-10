439cee97a18b529e00f3cc7080245900988906c6
Nigen <Nigen48@gmail.com>
2017-04-10 16:42:01 +0300

    Реструктуризация состояния

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,34 +1,17 @@
 import React from 'react';
 
-class Checkbox extends React.Component {
-  constructor (props) {
-    super(props);
+function Checkbox(props) {
 
-    this.state = {
-      checked: this.props.initiallyChecked
-    };
-
-this.handleClick = this.handleClick.bind(this);
-  }
-
-handleClick(event) {
-  this.setState ({
-    checked: !this.state.checked
-  });
-}
-
-  render() {
     return (
-      <button className="checkbox icon" onClick={this.handleClick}>
-        <i className="material-icons">{this.state.checked ? 'check_box' :
+      <button className="checkbox icon">
+        <i className="material-icons">{props.checked ? 'check_box' :
           'check_box_outline_blank'}</i>
         </button>
           );
         }
-      }
 
 Checkbox.propTypes = {
-  initiallyChecked: React.PropTypes.bool.isRequired
+  checked: React.PropTypes.bool.isRequired
 };
 
 export default Checkbox;
